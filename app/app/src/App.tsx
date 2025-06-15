abaf7bb2553fef15d4634e8d57a5d946dc69a730
Nguyen Tran (Mark) <nguyen.trantrung@navercorp.com>
2025-06-15 18:06:30 +0700

    added ipc to resize window

--- app/app/src/App.tsx
+++ app/app/src/App.tsx
@@ -1,12 +1,38 @@
 import type { Component } from "solid-js";
+import { createSignal } from "solid-js";
 
 import logo from "./logo.svg";
 import styles from "./App.module.css";
 
+declare global {
+  interface Window {
+    electronAPI?: {
+      resizeWindow: (width: number, height: number) => void;
+    };
+  }
+}
+
 const App: Component = () => {
+    const buttons = ["B1", "B2", "B3", "B4", "B5"];
+    const [showLeft, setShowLeft] = createSignal(false);
     return (
         <div class={styles.app}>
-            <h1>Hello, world1</h1>
+            {showLeft() && <div class={styles.leftSpace}>Hi</div>}
+            <div class={styles.menu}>
+                {buttons.map((label) => (
+                    <button
+                        class={styles.button}
+                        onClick={() => {
+                            if (label === "B1") {
+                                setShowLeft(true);
+                                window.electronAPI?.resizeWindow(520, 360);
+                            }
+                        }}
+                    >
+                        {label}
+                    </button>
+                ))}
+            </div>
         </div>
     );
 };
